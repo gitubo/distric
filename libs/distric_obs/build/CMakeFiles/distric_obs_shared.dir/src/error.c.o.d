428882f3846cd4d1b310b8618ea3d7b9bdb2fdb0
Mosca Massimiliano <m.mosca@almaviva.it>
2026-01-15 10:55:56 +0100

    removed build in distric_obs

--- libs/distric_obs/build/CMakeFiles/distric_obs_shared.dir/src/error.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/distric_obs_shared.dir/src/error.c.o: \
- /app/libs/distric_obs/src/error.c /usr/include/stdc-predef.h \
- /app/libs/distric_obs/include/distric_obs.h /usr/include/stddef.h \
- /usr/include/bits/alltypes.h /usr/include/stdint.h \
- /usr/include/bits/stdint.h /usr/include/stdbool.h
