2db251120c8d75bb0577ce7454e809e96fdd05c9
Mosca Massimiliano <m.mosca@almaviva.it>
2026-02-17 16:26:32 +0100

    Snapshot

--- libs/distric_obs/include/distric_obs/error.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISTRIC_OBS_ERROR_H
-#define DISTRIC_OBS_ERROR_H
-
-#include "distric_obs.h"
-
-/* Internal error handling - used only within the library */
-
-#endif /* DISTRIC_OBS_ERROR_H */
